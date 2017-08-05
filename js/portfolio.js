3d9a3fac0707b16886158f5f200af4ae4d063821
Berke KIRAN <berkekiranofficial@hotmail.com>
2017-08-05 15:10:17 +0300

    b update #1

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,86 @@
 // AngularJs Module and Controller Setup
 
 var app = angular.module("portfolio",['ngSanitize']);
+
+// Work Control System
+
+app.controller('WorkController', function($scope) {
+
+  // Works List
+
+  $scope.works = [
+                  {name: "Orc Leader", info: "2017 / 3D Character / Game-Ready", link: "https://www.artstation.com/artwork/2V4Lx", style : {"background-image" : "url('https://cdna.artstation.com/p/assets/images/images/006/823/800/large/berke-kiran-screenshot006.jpg?1501534299')", "background-size" : "cover", "background-position" : "center", "background-repeat" : "no-repeat" } },
+                  {name: "Sleeping Creature", info: "2016 / 3D Creature", link: "https://www.artstation.com/artwork/vdwLA", style : { "background-image" : "url('https://cdna.artstation.com/p/assets/images/images/006/823/646/large/berke-kiran-sleeping-creature.jpg?1501533327')", "background-size" : "cover", "background-position" : "center", "background-repeat" : "no-repeat" } },
+                  {name: "John", info: "2016 / 3D Character", link: "https://www.artstation.com/artwork/4Vd4k", style : { "background-image" : "url('https://cdnb.artstation.com/p/assets/images/images/006/842/405/large/berke-kiran-john-render-1.jpg?1501662674')", "background-size" : "cover", "background-position" : "center", "background-repeat" : "no-repeat" } },
+                  {name: "The God", info: "2016 / Digital Artwork", link: "https://www.artstation.com/artwork/Nm8g5", style : { "background-image" : "url('https://cdna.artstation.com/p/assets/images/images/006/805/672/large/berke-kiran-thegod.jpg?1501398937')", "background-size" : "cover", "background-position" : "center", "background-repeat" : "no-repeat" } },
+                 ];
+
+  var move = 0,
+      numOfSlides = $scope.works.length-1,
+      count = 0;
+
+  $scope.next = function() {
+    if(count < numOfSlides) {
+      if(count == 0) move -= 3320;
+      if(count >= 1) move -= 3320;
+      $('.works-ul').animate({"margin-left": move},1200);
+      count++;
+    }
+  };
+
+  $scope.prev = function() {
+    if(count == 0) move = 0;
+    if(count == 1) {move += 3320; count--;}
+    if(count >= 2) {move += 3320; count--;}
+    $('.works-ul').animate({"margin-left": move},1200);
+  };
+
+});
+
+// Load More Other Projects
+app.controller('OtherController', function($scope) {
+
+  var vm = this;
+  vm.other = others;
+  vm.limit = 6;
+
+  $scope.loadMore = function() {
+    var increamented = vm.limit + 3;
+    vm.limit = increamented > vm.other.length ? vm.other.length : increamented;
+
+    if(increamented >= vm.other.length){
+      $('.more').hide();
+    }
+  };
+
+});
+
+// Others List
+
+var others = [
+              { color: '#161616 ', link: 'https://github.com/berkekiran/portfolio-2017', name: 'Portfolio 2017', year: '2017', image: 'images/other-projects/portfolio-2017.png'},
+              { color: '#303030 ', link: 'https://github.com/berkekiran/portfolio-2016-v2', name: 'Portfolio 2016 v2', year: '2016', image: 'images/other-projects/portfolio-2016-v2.png'},
+              { color: '#a8933a ', link: 'https://github.com/berkekiran/video-games-center-station', name: 'Video Games Center Station', year: '2016', image: 'images/other-projects/video-games-center-station.png'},
+              { color: '#303030 ', link: 'https://github.com/berkekiran/portfolio-2016', name: 'Portfolio 2016', year: '2016', image: 'images/other-projects/portfolio-2016.png'},
+              { color: '#303030 ', link: 'https://github.com/berkekiran/portfolio-2015-v5', name: 'Portfolio 2015 v5', year: '2015', image: 'images/other-projects/portfolio-2015-v5.png'},
+              { color: '#cccccc ', link: 'https://www.artstation.com/artwork/ONdgg', name: 'UE4 Snowy Road', year: '2015', image: 'images/other-projects/snowyroad.png'},
+              { color: '#cccccc ', link: 'https://github.com/berkekiran/ue4-snow-system-v2', name: 'UE4 Snow System v2', year: '2015', image: 'images/other-projects/ue4-snow-system-v2.png'},
+              { color: '#cccccc ', link: 'https://github.com/berkekiran/ue4-snow-system-v1', name: 'UE4 Snow System v1', year: '2015', image: 'images/other-projects/ue4-snow-system-v1.png'},
+              { color: '#1D1F1E ', link: 'https://github.com/berkekiran/portfolio-2015-v4', name: 'Portfolio 2015 v4', year: '2015', image: 'images/other-projects/portfolio-2015-v4.png'},
+              { color: '#161616 ', link: 'https://github.com/berkekiran/portfolio-2015-v3-design', name: 'Portfolio 2015 v3 Design', year: '2015', image: 'images/other-projects/portfolio-2015-v3-design.png'},
+              { color: '#cccccc ', link: 'https://github.com/berkekiran/portfolio-2015-v2-design', name: 'Portfolio 2015 v2 Design', year: '2015', image: 'images/other-projects/portfolio-2015-v2-design.png'},
+              { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/berkekiran-truth', name: 'Berke Kiran - Truth', year: '2015', image: 'images/other-projects/berkekiran-truth.png'},
+              { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/berkekiran-death', name: 'Berke Kiran - Death', year: '2015', image: 'images/other-projects/berkekiran-death.png'},
+              { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/adam-noise-soundtrack-v3', name: 'Adam Noise - Soundtrack v3', year: '2015', image: 'images/other-projects/adam-noise-soundtrack-v3.png'},
+              { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/adam-noise-soundtrack-v2', name: 'Adam Noise - Soundtrack v2', year: '2015', image: 'images/other-projects/adam-noise-soundtrack-v2.png'},
+              { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/adam-noise-soundtrack-v1', name: 'Adam Noise - Soundtrack v1', year: '2015', image: 'images/other-projects/adam-noise-soundtrack-v1.png'},
+              { color: '#f8f8f8 ', link: 'https://vimeo.com/226496501', name: 'Metal Dragon - Video', year: '2015', image: 'images/other-projects/metal-dragon.png'},
+              { color: '#f8f8f8 ', link: 'https://github.com/berkekiran/portfolio-2015', name: 'Portfolio 2015', year: '2015', image: 'images/other-projects/portfolio-2015.png'},
+              { color: '#a7aaa9 ', link: 'https://github.com/berkekiran/photo-galleries', name: 'Photo Galleries', year: '2014', image: 'images/other-projects/photo-galleries.png'},
+              { color: '#444349 ', link: 'https://github.com/berkekiran/quick-write', name: 'Quick Write', year: '2013', image: 'images/other-projects/quick-write.png'}
+             ];
+
+
 app.controller("bodyController", function($scope, $timeout){
 
   // Page Scroll Speed Controller
@@ -112,68 +192,6 @@ app.controller("bodyController", function($scope, $timeout){
     }
   }
 
-  // Works List
-
-  $scope.works = [
-                  {workName: "orcleader", workHref: "https://www.artstation.com/artwork/2V4Lx", workStyle : {"background-image" : "url('images/works/orcleader.png')", "background-size" : "cover", "background-position" : "top", "background-repeat" : "no-repeat" } },
-                  {workName: "sleepingcreature", workHref: "https://www.artstation.com/artwork/vdwLA", workStyle : { "background-image" : "url('images/works/sleepingcreature.png')", "background-size" : "cover", "background-position" : "top", "background-repeat" : "no-repeat" } },
-                  {workName: "john", workHref: "https://www.artstation.com/artwork/4Vd4k", workStyle : { "background-image" : "url('images/works/john.png')", "background-size" : "cover", "background-position" : "top", "background-repeat" : "no-repeat" } },
-                  {workName: "thegod", workHref: "https://www.artstation.com/artwork/Nm8g5", workStyle : { "background-image" : "url('images/works/thegod.png')", "background-size" : "cover", "background-position" : "top", "background-repeat" : "no-repeat" } },
-                 ];
-
-  // Works slider system
-
-  var numOfSlides = $scope.works.length,
-      move = -40,
-      curSlide = 0;
-
-  function autoSlide() {
-    curSlide++;
-    if(curSlide <= numOfSlides){ changeSlides(); }
-    if(curSlide > numOfSlides) { autoSlideback(); }
-  };
-
-  function autoSlideback() {
-    move = -40;
-  };
-
-  function changeSlides() {
-    if(move == -40){ move = -1600; curSlide +=1; } else {move -= 1600;}
-    $('.works-ul').animate({"margin-left": move},1900);
-  };
-
-  window.addEventListener("scroll", function(){
-    var scrollTop = window.pageYOffset;
-    if(scrollTop >= 2200){
-    }else if(scrollTop < 2000){
-    }
-  }, true);
-
-  // Others List
-
-$scope.others = [
-  { color: '#161616 ', link: 'https://github.com/berkekiran/portfolio-2017', name: 'Portfolio 2017', year: '2017', image: 'images/other-projects/portfolio-2017.png'},
-  { color: '#303030 ', link: 'https://github.com/berkekiran/portfolio-2016-v2', name: 'Portfolio 2016 v2', year: '2016', image: 'images/other-projects/portfolio-2016-v2.png'},
-  { color: '#a8933a ', link: 'https://github.com/berkekiran/video-games-center-station', name: 'Video Games Center Station', year: '2016', image: 'images/other-projects/video-games-center-station.png'},
-  { color: '#303030 ', link: 'https://github.com/berkekiran/portfolio-2016', name: 'Portfolio 2016', year: '2016', image: 'images/other-projects/portfolio-2016.png'},
-  { color: '#303030 ', link: 'https://github.com/berkekiran/portfolio-2015-v5', name: 'Portfolio 2015 v5', year: '2015', image: 'images/other-projects/portfolio-2015-v5.png'},
-  { color: '#cccccc ', link: 'https://www.artstation.com/artwork/ONdgg', name: 'UE4 Snowy Road', year: '2015', image: 'images/other-projects/snowyroad.png'},
-  { color: '#cccccc ', link: 'https://github.com/berkekiran/ue4-snow-system-v2', name: 'UE4 Snow System v2', year: '2015', image: 'images/other-projects/ue4-snow-system-v2.png'},
-  { color: '#cccccc ', link: 'https://github.com/berkekiran/ue4-snow-system-v1', name: 'UE4 Snow System v1', year: '2015', image: 'images/other-projects/ue4-snow-system-v1.png'},
-  { color: '#1D1F1E ', link: 'https://github.com/berkekiran/portfolio-2015-v4', name: 'Portfolio 2015 v4', year: '2015', image: 'images/other-projects/portfolio-2015-v4.png'},
-  { color: '#161616 ', link: 'https://github.com/berkekiran/portfolio-2015-v3-design', name: 'Portfolio 2015 v3 Design', year: '2015', image: 'images/other-projects/portfolio-2015-v3-design.png'},
-  { color: '#cccccc ', link: 'https://github.com/berkekiran/portfolio-2015-v2-design', name: 'Portfolio 2015 v2 Design', year: '2015', image: 'images/other-projects/portfolio-2015-v2-design.png'},
-  { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/berkekiran-truth', name: 'Berke Kiran - Truth', year: '2015', image: 'images/other-projects/berkekiran-truth.png'},
-  { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/berkekiran-death', name: 'Berke Kiran - Death', year: '2015', image: 'images/other-projects/berkekiran-death.png'},
-  { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/adam-noise-soundtrack-v3', name: 'Adam Noise - Soundtrack v3', year: '2015', image: 'images/other-projects/adam-noise-soundtrack-v3.png'},
-  { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/adam-noise-soundtrack-v2', name: 'Adam Noise - Soundtrack v2', year: '2015', image: 'images/other-projects/adam-noise-soundtrack-v2.png'},
-  { color: '#f8f8f8 ', link: 'https://soundcloud.com/berkekiran/adam-noise-soundtrack-v1', name: 'Adam Noise - Soundtrack v1', year: '2015', image: 'images/other-projects/adam-noise-soundtrack-v1.png'},
-  { color: '#f8f8f8 ', link: 'https://vimeo.com/226496501', name: 'Metal Dragon - Video', year: '2015', image: 'images/other-projects/metal-dragon.png'},
-  { color: '#f8f8f8 ', link: 'https://github.com/berkekiran/portfolio-2015', name: 'Portfolio 2015', year: '2015', image: 'images/other-projects/portfolio-2015.png'},
-  { color: '#a7aaa9 ', link: 'https://github.com/berkekiran/photo-galleries', name: 'Photo Galleries', year: '2014', image: 'images/other-projects/photo-galleries.png'},
-  { color: '#444349 ', link: 'https://github.com/berkekiran/quick-write', name: 'Quick Write', year: '2013', image: 'images/other-projects/quick-write.png'}
-];
-
   // Social Buttons List
 
   $scope.socialLinks = [{ socialName: "linkedin", socialHref: "https://www.linkedin.com/in/berkekiran/", socialSrc : "images/social-buttons/linkedin-logo.svg"},
@@ -185,7 +203,8 @@ $scope.others = [
                         { socialName: "instagram", socialHref: "https://www.instagram.com/berkekiran.official/", socialSrc : "images/social-buttons/instagram-logo.svg"},
                         { socialName: "facebook", socialHref: "https://www.facebook.com/xBerkeKiranx", socialSrc : "images/social-buttons/facebook-logo.svg"}
                        ];
-});
+  });
+
 
 // Other Javascript Codes
 
